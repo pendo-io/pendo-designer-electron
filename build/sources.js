0fcdecc6a75b862c8eb23f8f6df192d1d85d0db7
Jonathan Fontanez <jfontanez@pendo.io>
2017-04-13 15:19:22 -0400

    APP-2803 make electron great and work with remote deployments

--- build/sources.js
+++
@@ -1,5 +0,0 @@
-exports.agent = 'pendo-designer-agent';
-exports.background = 'pendo-designer-background';
-exports.contentScript = 'pendo-designer-content-script';
-exports.designer = 'pendo-designer';
-exports.plugin = 'pendo-designer-plugin';
